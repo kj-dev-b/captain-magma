36229e2e4d868c01cdc0b477808cf03243bc1134
KeegJordan <keeganbbjordan@gmail.com>
2019-08-29 12:24:51 -0700

    initial commit

---
+++ src/Home.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import logo from './Captain_Magma.jpg';
+import './Home.css';
+
+class Home extends React.Component {
+  render() {
+    return (
+      <div className="Home">
+        <div className="nav-bar"> 
+          <ul className="nav-list">
+            <li><a href="/">Home</a></li>
+            <li><a href="/about">About</a></li>
+            <li><a href="/server">MCServer</a></li>
+          </ul>
+        </div>
+        <div className="Home-header">
+          <img src={logo} className="logo" alt="cm" />
+          <h2>CAPTAIN MAGMA</h2>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Home;
